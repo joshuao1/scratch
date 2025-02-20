3baf45f3fe4e8b196805d15137096fc77a8186ef
Josh Ong <js.joshuaong@gmail.com>
2025-02-20 19:43:33 +1100

    made it take query body

--- main.py
+++ main.py
@@ -7,30 +7,34 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+
 load_dotenv()
 
 deep_seek_api_key = os.getenv("DEEPSEEK_API")
-
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key=deep_seek_api_key,
 )
 
-completion = client.chat.completions.create(
-  extra_headers={
-    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
-    "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
-  },
-  extra_body={},
-  model="deepseek/deepseek-chat:free",
-  messages=[
-    {
-      "role": "user",
-      "content": "Give me another cat fact"
-    }
-  ]
-)
+# completion = client.chat.completions.create(
+#   extra_headers={
+#     "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+#     "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+#   },
+#   extra_body={},
+#   model="deepseek/deepseek-chat:free",
+#   messages=[
+#     {
+#       "role": "user",
+#       "content": "Give me another cat fact"
+#     }
+#   ]
+# )
 # print(completion.choices[0].message.content)
 
 app = FastAPI()
@@ -51,6 +55,25 @@ def read_root():
             }
         ]
 )
+    return {completion.choices[0].message.content}
+
+
+@app.post("/question/")
+async def create_item(item: Item):
+    print(item.name)
+    completion = client.chat.completions.create(
+        extra_headers={
+            "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+            "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+        },
+        extra_body={},
+        model="deepseek/deepseek-chat:free",
+        messages=[
+            {
+            "role": "user",
+            "content": item.name
+            }
+        ])
     return {completion.choices[0].message.content}
 
 @app.get("/items/{item_id}")
